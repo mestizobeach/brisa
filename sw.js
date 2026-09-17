4332c93bc936406e4e7c816658a7b1876ae5ca67
mestizobeach <mestizobeach@gmail.com>
2026-09-17 20:45:05 +0200

    Refresh app cache after design update

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener("install", (event) => event.waitUntil(caches.open("brisa-v1").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"]))));
+self.addEventListener("install", (event) => event.waitUntil(caches.open("brisa-v2").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"]))));
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
