077305933fc2fbdb302be16309c5857112f420db
mestizobeach <mestizobeach@gmail.com>
2026-09-17 23:36:56 +0200

    Refresh app cache for tides card

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "brisa-v8";
-const ASSETS = ["./", "./index.html", "./styles.css", "./hourly.css", "./sunset.css", "./food.css", "./app.js", "./manifest.webmanifest"];
+const CACHE = "brisa-v9";
+const ASSETS = ["./", "./index.html", "./styles.css", "./hourly.css", "./sunset.css", "./food.css", "./tides.css", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
