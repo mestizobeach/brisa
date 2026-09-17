41493942b512cceef5dd8f8f85d38b7e03869147
mestizobeach <mestizobeach@gmail.com>
2026-09-17 22:59:50 +0200

    Refresh cache for hourly forecast

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener("install", (event) => event.waitUntil(caches.open("brisa-v5").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"]))));
+self.addEventListener("install", (event) => event.waitUntil(caches.open("brisa-v6").then((cache) => cache.addAll(["./", "./index.html", "./styles.css", "./hourly.css", "./app.js", "./manifest.webmanifest"]))));
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
